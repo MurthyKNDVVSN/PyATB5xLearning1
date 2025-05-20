b5eb4d5cd177cff7283365cdc5fda2cd8f17ef97
satya <murthy.kandavalli@gmail.com>
2025-05-20 08:39:33 +0530

    May 19 th new files

---
+++ MAY/Ex_May20_Functions/lab64_functions2.py
@@ -0,0 +1,32 @@
+import math
+
+# Built Functions (Python Guys)
+# math.pow(2,3)
+# input()
+# print()
+# len()
+# float()
+# int()
+# str()
+# math.factorial()
+# pow()
+# type()
+# divmod()#
+# bool()
+# max()
+# min()
+# random()
+#  str.lower()
+# str.upper()
+
+
+# User Defined functions
+def say_hello():
+    print("Hi")
+
+say_hello()
+
+def test_hello():
+    print("Hello how are you")
+test_hello()
+test_hello()
